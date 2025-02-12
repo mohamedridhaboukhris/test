545048f5e582bf6c3910ea1ce0435b4258063894
Tayeb <taieb.benkhadhra@esprit.tn>
2025-02-12 19:01:55 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { DonateComponent } from './donate/donate.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'contact', component: ContactComponent },
+  { path: 'how-it-works', component: HowItWorksComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'blog_details', component: BlogDetailsComponent},
+  { path: 'donate', component: DonateComponent},
+  { path: 'gallery', component: GalleryComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+
+import { GalleryComponent } from './gallery/gallery.component';
+import { DonateComponent } from './donate/donate.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    NavbarComponent,
+    HowItWorksComponent,
+    AboutComponent,
+    BlogComponent,
+    GalleryComponent,
+    DonateComponent,
+    BlogDetailsComponent,
+    DashboardComponent
+  
   ],
   imports: [
     BrowserModule,
